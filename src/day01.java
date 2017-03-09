7d44447b3702e22d1320d730c8d116b511f5981d
zhichao19930808 <zhichaoglory@163.com>
2017-03-09 15:03:10 +0800

    day01

--- src/day01.java
+++ src/day01.java
@@ -1,34 +1,16 @@
-import com.sun.org.apache.xpath.internal.SourceTree;
-
-import java.util.Scanner;
-
 /**
  * Created by Administrator on 2017/3/7.
  */
 public class day01 {
     public static void main(String[] args) {
-//数学
-        int math;
-        //C语言
-        int c;
-        //物理
-        int phycal;
-        //英语
-        int english;
-        Scanner input = new Scanner(System.in);
-        System.out.println("请输入数学的成绩");
-         math = input.nextInt();
-        System.out.println("请输入c语言的成绩");
-        c = input.nextInt();
-        System.out.println("请输入英语的成绩");
-        english = input.nextInt();
-        System.out.println(math+c+english);
-        if(math>c){
-            System.out.println("数学跟c语言中成绩比较高的分数为"+"数学"+math);
-        }else{
-            System.out.println("数学跟c语言中成绩比较高的分数为"+"c语言"+c);
-        }
+        //.../
+        int a = 7;
+        int b = 10;
 
+        int c = a*b;
 
+       System.out.print(c);
     }
 }
+//public 公众的   static 静态的   void  空的   main  主干  string  串 args
+//System  系统 out 出  print 印刷
