844a71c148278dfb8482a4f46737c217c07d86f5
zhichao19930808 <zhichaoglory@163.com>
2017-03-20 15:10:48 +0800

    if else练习

---
+++ src/if_else_b.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Created by Administrator on 2017/3/20.
+ */
+//根据用户输入的月份，打印出相应的季节。
+//345春季 678 夏季 9 10 11秋季 12 1 2
+public class if_else_b {
+    //主函数
+    public static void main(String[]args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("请输入月份");
+        byte x=input.nextByte();
+        if (x > 12 || x < 1) {
+            System.out.print("当前月份不存在");
+        } else if (x >= 3 && x <= 5) {
+            System.out.print(x+"月份是春季");
+        } else if (x >= 6 && x <= 8) {
+            System.out.print(x+"月份是夏季");
+        } else if (x >= 9 && x <= 11) {
+            System.out.print(x + "月是秋季");
+        } else {
+            System.out.print(x+"月是冬季");
+        }
+    }
+}
