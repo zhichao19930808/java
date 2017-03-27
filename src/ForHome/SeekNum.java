4e6a9aaace543cd4d29231c265c0f0fe70002812
zhichao19930808 <zhichaoglory@163.com>
2017-03-27 16:35:55 +0800

    输出  100 -- 999之间, 所有百位 + 十位 == 个位　的数

--- src/ForHome/SeekNum.java
+++ src/ForHome/SeekNum.java
@@ -6,8 +6,14 @@ package ForHome;
 public class SeekNum {
     //输出  100 -- 999之间, 所有百位 + 十位 == 个位　的数
     public static void main(String[] args) {
-        int x,y,z;//定义百位、十位、个位
+        int x,y,z;//定义个位、十位、百位
         for (int num = 100;num<=999;num++) {
+            x=num%10;
+            y=(num/10)%10;
+            z=(num/100)%10;
+            if (y + z == x) {
+                System.out.println(num);
+            }
 
         }
     }
