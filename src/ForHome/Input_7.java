96f830faf1fdf2db5f10adde4ddf5f5a625cbfbe
zhichao19930808 <zhichaoglory@163.com>
2017-03-27 11:05:47 +0800

    For

---
+++ src/ForHome/Input_7.java
@@ -0,0 +1,13 @@
+package ForHome;
+
+/**
+ * Created by Administrator on 2017/3/27.
+ */
+public class Input_7 {
+    //输出　100 -- 300之间, 所有个位为7的数
+    public static void main(String[] args) {
+        for (int x =7;x<300;x+=10) {
+            System.out.println(x);
+        }
+    }
+}
---
+++ src/ForHome/SeekNum.java
@@ -0,0 +1,14 @@
+package ForHome;
+
+/**
+ * Created by Administrator on 2017/3/27.
+ */
+public class SeekNum {
+    //输出  100 -- 999之间, 所有百位 + 十位 == 个位　的数
+    public static void main(String[] args) {
+        int x,y,z;//定义百位、十位、个位
+        for (int num = 100;num<=999;num++) {
+
+        }
+    }
+}
