4dcab42e4c14bc6bce45c1b76916523d7e0c5d8b
zhichao19930808 <zhichaoglory@163.com>
2017-03-28 13:57:05 +0800

    有如下规律的一组数字，求该组数字的第37位,并找出233在该数组第几位。1，1 ， 2	，3， 5，8， 13，21，34， 55

--- src/ArrayHome/ArrayText_a.java
+++ src/ArrayHome/ArrayText_a.java
@@ -1,19 +1,22 @@
+package ArrayHome;
+
 /**
  * Created by Administrator on 2017/3/28.
  */
 public class ArrayText_a {
     //array 数组
     public static void main(String[] args) {
-        //	有如下规律的一组数字，求该组数字的第37位。
-      //  1	 1 	    2	    3	    5
-      //  8	 13   21	    34	    55
+        //	有如下规律的一组数字，求该组数字的第37位,并找出233在该数组第几位。1，1 ， 2	，3， 5，8， 13，21，34， 55
         int[] array = new  int[37];
         array[0] = 1;//第一个数；
         array[1] = 1;//第二个数；
         for ( int num = 2;num<37;num++) {
             //第三个数等于第一个数加上第二个数
             array[num] = array[num-1]+array[num-2];
+            if (array[num] == 233) {
+                System.out.println("233在该数组中是第"+num+"位");
+            }
         }
-        System.out.println(array[36]);
+        System.out.println("该数组的第37位是："+array[36]);
     }
 }
