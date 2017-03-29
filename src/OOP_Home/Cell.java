247b8247a5b9231535305b04c6c6706871f430a7
zhichao19930808 <zhichaoglory@163.com>
2017-03-29 16:52:57 +0800

    写电池类（Cell）：具有品牌属性，可以续电

---
+++ src/OOP_Home/Cell.java
@@ -0,0 +1,13 @@
+package OOP_Home;
+
+/**
+ * Created by ZhiChao on 2017/3/29.
+ * 编写电池类（Cell）：具有品牌属性，可以续电
+ */
+    public class Cell {
+    //属性
+    String brand;//品牌
+    public void TextCell() {
+        System.out.println("续电");
+    }
+}
