dd76d5e3b36e6b731eedf932bffae89585188fde
zhichao19930808 <zhichaoglory@163.com>
2017-04-01 15:41:26 +0800

    Java 集合类试题

--- src/Exam_3.java
+++ src/Exam_3.java
@@ -11,9 +11,9 @@ public class Exam_3 {
     //定义一个方法，实现数组的转置输出
     //例如： 源数组，1 2 3 4 5 6 转置之后的数组，6 5 4 3 2 1
     //首先要自定义一组数组并为其赋值
-    int[]a= new int[9];
+    int[]a= new int[5];
     Scanner input =new Scanner(System.in);
-    for (int i=0;i<9;i++) {
+    for (int i=0;i<5;i++) {
         System.out.println("请输入第个"+(i+1)+"数字");
         int x = input.nextInt();
         a[i] = x;
